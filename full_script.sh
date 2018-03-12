e807d4e812a309533171889b1b4cca0d35a77871
Kalaboum <nataniel.hofer@gmail.com>
2018-03-12 17:16:54 +0100

    fixing the script, should work out of the box

--- full_script.sh
+++ full_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mkdir data
-FILE_NAME_USED = "geometryroute"
-EXTENSION = "_light.json"
-mv $FILE_NAME_USED.* ./data
+FILE_NAME_USED=routegeometry
+EXTENSION=_light.json
+mv "$FILE_NAME_USED.shp" "$FILE_NAME_USED.shx" "$FILE_NAME_USED.prj" "$FILE_NAME_USED.dbf" ./data
 cd data
-ogr2ogr -f "GeoJSON" $FILE_NAME_USED$EXTENSION $FILE_NAME_USED.shp -s_srs EPSG:21781 -t_srs EPSG:4326 -select land,route_id, surface
+ogr2ogr -f "GeoJSON" $FILE_NAME_USED$EXTENSION $FILE_NAME_USED.shp -s_srs EPSG:21781 -t_srs EPSG:4326 -select land,route_id,surface
 tippecanoe -o wander_velo_spec.mbtiles -S 15 --drop-densest-as-needed --minimum-zoom=5 --maximum-zoom=16 $FILE_NAME_USED$EXTENSION
 cd ..
 ./docker_script.sh
