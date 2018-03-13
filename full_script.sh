c686cddcd2e8d69be54766cd0770c0cd422b723b
Kalaboum <nataniel.hofer@gmail.com>
2018-03-13 11:19:54 +0100

    Building via docker-compose

--- full_script.sh
+++ full_script.sh
@@ -7,4 +7,4 @@ cd data
 ogr2ogr -f "GeoJSON" $FILE_NAME_USED$EXTENSION $FILE_NAME_USED.shp -s_srs EPSG:21781 -t_srs EPSG:4326 -select land,route_id,surface
 tippecanoe -o wander_velo_spec.mbtiles -S 15 --drop-densest-as-needed --minimum-zoom=5 --maximum-zoom=16 $FILE_NAME_USED$EXTENSION
 cd ..
-./docker_script.sh
+docker-compose up
